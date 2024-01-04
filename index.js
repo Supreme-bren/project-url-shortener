820420abe6d774f213a5739772dd730bc5384c1e
Brennan <brennanosunkwo14@gmail.com>
2024-01-03 22:53:36 -0500

    completed url-shortener project

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const urlSchema = new mongoose.Schema({
+  original: {
+    type: String,
+    required: true
+  },
+  short: Number
+});
+
+const Url = mongoose.model('test', urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -16,58 +29,55 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+let resObject = {};
+app.post('/api/shorturl', bodyParser.urlencoded({extended: false}), function(req, res) {
+  let inputUrl = req.body.url
+
+  let regExpression = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/ig);
+
+  if(!inputUrl.match(regExpression)){
+    res.json({
+      error: "invalid url"
+    })
+    return;
+  }
+
+
+  resObject['original_url'] = inputUrl;
+
+  let shortInput = 1;
+  Url.findOne({}).sort({short: 'desc'}).exec((error, result) =>{
+    if(!error && result != undefined){
+      shortInput = result.short + 1
+    }
+    if(!error){
+      Url.findOneAndUpdate(
+        {original: inputUrl},
+        {original: inputUrl, short: shortInput},
+        {new: true, upsert: true},
+        (error, savedUrl)=> {
+          if(!error){
+            resObject['short_url'] = savedUrl.short;
+            res.json(resObject);
+          }
+        }
+      );
+    }
+  })
 });
+
+app.get('/api/shorturl/:input', (req, res) =>{
+  let input = req.params.input;
+  Url.findOne({short: input}, (err, result) =>{
+    if(!err && result != undefined){
+      res.redirect(result.original);
+    }
+    else{
+      res.json({error: 'url not found'})
+    }
+  })
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
-});
-
-const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://osunkbr:Rtx3080FE@cluster0.jmlmiz9.mongodb.net/?retryWrites=true&w=majority");
-
-const User = require('./server')
-let originalUrls = [];
-let shortUrls = [];
-
-app.post('/api/shorturl', bodyParser.urlencoded({extended: false}), (req, res) =>{
-
-  const url = req.body.url
-
-  const indexSpot = originalUrls.indexOf(url)
-
-  if(!url.includes("https://" || !url.includes("http://"))){
-    res.json({
-      error: 'invalid url'
-    })
-  }
-
-  if(indexSpot < 0){
-    originalUrls.push(url)
-    shortUrls.push(shortUrls.length)
-    res.json({
-      original_url: url,
-      short_url: shortUrls.length - 1
-    })
-  }
-  else{
-    res.json({
-      original_url: url,
-      short_url: shortUrls[indexSpot] 
-    })
-  }
-  
-})
-app.get('/api/shorturl/:shorturl', (req, res) =>{
-  const shortUrl = parseInt(req.params.shorturl)
-  const newIndexSpot = shortUrls.indexOf(shortUrl)
-  if(newIndexSpot < 0){
-    res.json({
-      error: "No short URL found for the given input"
-    })
-  }
-  else{
-    res.redirect(originalUrls[newIndexSpot])
-  }
 })
